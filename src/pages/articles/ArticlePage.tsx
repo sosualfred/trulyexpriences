34109f4f8c1ec3d88baf45147e4e311025afaa8d
sosualfred <sosualfred@gmail.com>
2023-03-05 14:15:09 +0000

    update article page

--- src/pages/articles/ArticlePage.tsx
+++ src/pages/articles/ArticlePage.tsx
@@ -15,22 +15,65 @@ export default function ArticlePage() {
   }
 
   return (
-    <div className="container mx-auto h-screen">
-      {/* Artilce Image, Title, Description, and Iframe to show link */}
-      <img
-        className="w-full h-60 object-cover"
-        src={article.urlToImage || "https://picsum.photos/200"}
-        alt="Sunset in the mountains"
-      />
-      <div className="px-6 py-4">
-        <div className="font-bold text-gray-700 mb-2 text-ellipsis line-clamp-2">
-          {article.title || "No title"}
+    <>
+      <div className="container mx-auto h-screen">
+        <div className=" px-10 py-6 mx-auto">
+          <div className="max-w-6xl px-10 py-6 mx-auto">
+            <a
+              href="#_"
+              className="block transition duration-200 ease-out transform"
+            >
+              <img
+                className="object-cover w-full shadow-sm h-full"
+                src={article.urlToImage || "https://picsum.photos/200"}
+              />
+            </a>
+
+            {/* Date Published */}
+            {article.publishedAt && (
+              <div className="flex justify-between items-center mt-4">
+                <span className="font-light text-gray-600">
+                  {new Date(article.publishedAt).toDateString()}
+                </span>
+              </div>
+            )}
+
+            <div className="mt-2">
+              <a
+                href={article.url}
+                target="_blank"
+                className="sm:text-3xl md:text-3xl lg:text-3xl xl:text-4xl font-bold text-purple-500  hover:underline"
+              >
+                {article.title}
+              </a>
+
+              <h1 className="font-bold text-gray-700 mt-2">
+                Source{" "}
+                <span className="text-gray-500">{article.source.name}</span>
+              </h1>
+
+              <h1 className="font-bold text-gray-700 mt-2">
+                By{" "}
+                <span className="text-gray-500">
+                  {article.author || "Unknown"}
+                </span>
+              </h1>
+            </div>
+
+            <div className="max-w-4xl px-10  mx-auto text-2xl text-gray-700 mt-4 rounded bg-gray-100">
+              <div>
+                <p className="mt-2 p-8">{article.description}</p>
+              </div>
+            </div>
+
+            <div className="mx-auto text-gray-700 mt-4 rounded bg-gray-100">
+              <div>
+                <p className="mt-2 p-8">{article.content}</p>
+              </div>
+            </div>
+          </div>
         </div>
-        <p className="text-gray-700 text-base text-ellipsis line-clamp-1">
-          {article.description || "No description"}
-        </p>
       </div>
-      <iframe src={article.url} title={article.title}></iframe>
-    </div>
+    </>
   );
 }
